ac720928e5a5b5c57defa40aa468f30666f54c3c
Zein-praft <zeinfahmi449@gmail.com>
2025-07-31 14:35:21 +0400

    memperbaiki tabel categories

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\item;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,7 @@ class ItemSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        item::factory()->count(10)->create();
+            
     }
 }
