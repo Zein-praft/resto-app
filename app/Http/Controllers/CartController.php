06504c6389a485d1a1a1baaab85b2d15df95db0f
Zein-praft <zeinfahmi449@gmail.com>
2025-08-07 06:57:15 +0400

    memperbaiki jalaman cart(tombol kurang dan tambah makanan)

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+
+class CartController extends Controller
+{
+    public function add(Request $request)
+    {
+        $cart = session()->get('cart', []);
+        $item = Item::findOrFail($request->item_id);
+
+        if (isset($cart[$item->id])) {
+            $cart[$item->id]['qty']++;
+        } else {
+            $cart[$item->id] = [
+                'name' => $item->name,
+                'price' => $item->price,
+                'img' => $item->img,
+                'qty' => 1,
+            ];
+        }
+        session(['cart' => $cart]);
+        return redirect()->back()->with('success', 'Berhasil ditambahkan ke keranjang!');
+    }
+    public function show()
+    {
+        $cart = session()->get('cart', []);
+        return view('customer.cart', compact('cart'));
+    }
+
+    public function updateQty(Request $request, $id)
+    {
+        $cart = session()->get('cart', []);
+
+        if (isset($cart[$id])) {
+            $newQty = (int) $request->qty;
+            if ($newQty > 0) {
+                $cart[$id]['qty'] = $newQty;
+            } else {
+                unset($cart[$id]);
+            }
+        }
+
+        session(['cart' => $cart]);
+        return redirect()->route('cart'); // cart.show jadi cart aja 
+    }
+
+    public function remove($id)
+    {
+        $cart = session()->get('cart', []);
+
+        if (isset($cart[$id])) {
+            unset($cart[$id]);
+        }
+
+        session(['cart' => $cart]);
+        return redirect()->route('cart');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,28 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Item;
 
-Route::get('/', function () {
-    return view('customer.menu');
-});
-Route::get('/cart', function () {
-    return view('customer.cart');
-})->name('cart');
+// Route menu
+Route::get('/menu', [ItemController::class, 'index']);
+
+// Route home (menu utama)
+Route::get('/', [ItemController::class, 'index']);
+
+// Route::get('/cart', function () {
+//     return view('customer.cart');
+// })->name('cart');
+
+// Route checkout
 Route::get('/checkout', function () {
     return view('customer.checkout');
 })->name('checkout');
+
+// Cart routes
+Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
+Route::get('/cart', [CartController::class, 'show'])->name('cart');
+Route::post('/cart/update/{id}', [CartController::class, 'updateQty'])->name('cart.update');
+Route::post('/cart/remove/{id}', [CartController::class, 'remove'])->name('cart.remove');
+
